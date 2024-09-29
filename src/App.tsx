9f54be3b2dc0494a2b8d1eeba575d6b593a28a0e
ibrahimkayum18 <ibrahimkayum017@gmail.com>
2024-09-29 22:28:22 +0600

    Added sign in the web page

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,44 @@
-// import { useDispatch, useSelector } from "react-redux";
 import { decrement, increment } from "./redux/features/counterSlice";
 import { useAppDispatch, useAppSelector } from "./redux/hook";
 
 function App() {
   const count = useAppSelector((state) => state.counter.count);
-  console.log(count);
   const dispatch = useAppDispatch();
+
+  let dividedNumber: number = 0;
+
+  if (count) {
+    dividedNumber = Math.floor(count / 5) * 5;
+  }
+
+  const signGroups = [];
+  for (let i = 0; i < dividedNumber; i += 5) {
+    signGroups.push(
+      <span key={i} className="pr-5">
+        {"|".repeat(Math.min(5, dividedNumber - i))}
+      </span>
+    );
+  }
 
   return (
     <>
       <div className="flex justify-center items-center gap-5 h-screen">
-        <button
-          onClick={() => dispatch(increment(5))}
-          className="px-8 py-3 rounded-md bg-green-700 text-white"
-        >
-          Increment
-        </button>
-        <h1 className="text-4xl mx-8">{count}</h1>
-        <button
-          onClick={() => dispatch(decrement(5))}
-          className="px-8 py-3 rounded-md bg-red-700 text-white"
-        >
-          Decrement
-        </button>
+        <div className="flex justify-center items-center gap-5 p-10 border-2 border-black">
+          <button
+            onClick={() => dispatch(increment(1))}
+            className="px-8 py-3 rounded-md bg-green-700 text-white"
+          >
+            Increment
+          </button>
+          <h1 className="text-4xl mx-8">{count}</h1>
+          <button
+            onClick={() => dispatch(decrement(1))}
+            className="px-8 py-3 rounded-md bg-red-700 text-white"
+          >
+            Decrement
+          </button>
+        </div>
+        <div>{signGroups}</div>
       </div>
     </>
   );
